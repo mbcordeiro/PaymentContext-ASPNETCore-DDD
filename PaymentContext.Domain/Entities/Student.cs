2c8c14173b91648e1cc26d0aa223eb8e5e3769de
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-01-05 00:39:04 -0300

    Design by contracts

--- PaymentContext.Domain/Entities/Student.cs
+++ PaymentContext.Domain/Entities/Student.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using Flunt.Validations;
 using PaymentContext.Domain.ValueObjects;
 using PaymentContext.Shared.Entities;
 
@@ -22,17 +23,25 @@ namespace PaymentContext.Domain.Entities
         public Document Document { get; private set; }
         public Email Email { get; private set; }
         public Address Address { get; private set; }
-        public IReadOnlyCollection<Subscription> Subscriptions
-        {
-            get { return _subscriptions.ToArray(); }
-        }
-
+        public IReadOnlyCollection<Subscription> Subscriptions { get { return _subscriptions.ToArray(); } }
+        
         public void AddSubscription(Subscription subscription)
         {
-            foreach (var sub in Subscriptions)
-                sub.Activate(false);
+            var hasSubscriptionActive = false;
+            foreach (var sub in _subscriptions)
+            {
+                if (sub.Active)
+                    hasSubscriptionActive = true;
+            }
 
-            _subscriptions.Add(subscription);
+            AddNotifications(new Contract()
+                .Requires()
+                .IsFalse(hasSubscriptionActive, "Student.Subscriptions", "Você já tem uma assinatura ativa")
+                .AreNotEquals(0, subscription.Payments.Count, "Student.Subscription.Payments", "Esta assinatura não possui pagamentos")
+            );
+
+            if (Valid)
+                _subscriptions.Add(subscription);
         }
     }
 }
--- PaymentContext.Domain/ValueObjects/Name.cs
+++ PaymentContext.Domain/ValueObjects/Name.cs
@@ -1,19 +1,24 @@
+using Flunt.Validations;
 using PaymentContext.Shared.ValueObjects;
 
 namespace PaymentContext.Domain.ValueObjects
 {
     public class Name : ValueObject
     {
-        public Name(string firstName, string lasttName)
+        public Name(string firstName, string lastName)
         {
             FirstName = firstName;
-            LasttName = lasttName;
+            LastName = lastName;
 
-            if (string.IsNullOrEmpty(FirstName))
-                AddNotification("Name.FirtName", "Nome inválido1");
+            AddNotifications(new Contract()
+                .Requires()
+                .HasMinLen(FirstName, 3, "Name.FirstName", "Nome deve conter pelo menos 3 caracteres")
+                .HasMinLen(LastName, 3, "Name.LastName", "Sobrenome deve conter pelo menos 3 caracteres")
+                .HasMaxLen(FirstName, 40, "Name.FirstName", "Nome deve conter até 40 caracteres")
+            );
         }
 
         public string FirstName { get; private set; }
-        public string LasttName { get; private set; }
+        public string LastName { get; private set; }
     }
 }
